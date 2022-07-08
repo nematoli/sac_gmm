93615320c02a01983ded0f724307f7cd823565ed
Iman Nematollahi <nematollahi.iman@gmail.com>
2022-07-08 15:41:55 +0200

    repo configurations

---
+++ sac_gmm/__init__.py
@@ -0,0 +1,10 @@
+"""Robot Skill Adaptation via Soft Actor-Critic Gaussian Mixture Models implementation in pytorch
+:copyright: 2022 by Iman Nematollahi
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "1.0.0"
+__project__ = "SAC-GMM"
+__author__ = "Iman Nematollahi"
+__license__ = "MIT"
+__email__ = "nematoli@cs.uni-freiburg.de"
