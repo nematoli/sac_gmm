fixed the hydra issue, added velocity calculation

sac_gmm/extract_demos.py
@@ -47,7 +47,7 @@ def save_demonstrations(datamodule, save_dir):
         np.save(save_dir, demos)
 
 
-@hydra.main(config_path="../config", config_name="demos", version_base=None)
+@hydra.main(config_path="../config", config_name="demos")
 def extract_demos(cfg: DictConfig) -> None:
     """
     This is called to extract demonstrations for a specific skill.

sac_gmm/gmm_train.py
@@ -1,3 +1,4 @@
+from distutils.command.config import config
 import os
 import sys
 import logging
@@ -7,7 +8,7 @@ from tqdm import tqdm
 import numpy as np
 import hydra
 from hydra import initialize, compose
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 from extract_demos import extract_demos
 from utils.env_maker import make_env
@@ -34,18 +35,21 @@ class GMMTrainer(object):
             raise Exception('GMM type not supported')
 
     def fit_gmm(self, data):
+        logger.info(f'Fitting {self.cfg.gmm_type} GMM with {self.cfg.gmm_components} components')
         gmm = self.gmm(n_components=self.cfg.gmm_components, random_state=self.cfg.seed)
         return gmm.from_samples(X=data)
 
-    def evaluate(self, gmm):
+    def evaluate(self, gmm, data):
         return None
 
 
     def run(self):
         # Extract demonstrations
         if self.cfg.use_existing_demos:
+            print(Path().absolute())
+            print((Path(self.cfg.demos_dir) / self.cfg.skill / 'train.npy').is_file())
             if (Path(self.cfg.demos_dir) / self.cfg.skill / 'train.npy').is_file() &\
-                (Path(self.cfg.demos_dir) / self.cfg.skill / 'val.npy').is_file():
+                            (Path(self.cfg.demos_dir) / self.cfg.skill / 'val.npy').is_file():
                 logger.info(f'Using demonstration available at {self.cfg.demos_dir}/{self.cfg.skill}')
             else:
                 raise Exception(f'Missing demonstrations at {Path(self.cfg.demos_dir) / self.cfg.skill}')
@@ -56,19 +60,32 @@ class GMMTrainer(object):
         train_data = np.load(Path(self.cfg.demos_dir) / self.cfg.skill / 'train.npy')
         train_data = train_data.reshape(1, -1, train_data.shape[-1]).squeeze(0)
         val_data = np.load(Path(self.cfg.demos_dir) / self.cfg.skill / 'val.npy')
-        fitted_gmm = self.fit_gmm(train_data)
+        # Get velocities from pose
+        dt = 1.0
+        X_dot = (train_data[2:] - train_data[:-2]) / dt
+        X = train_data[1:-1]
+        X_train = np.hstack((X, X_dot))
+        fitted_gmm = self.fit_gmm(X_train)
 
         # Evaluate in Calvin environment
-        self.evaluate(fitted_gmm)
+        self.evaluate(fitted_gmm, val_data)
 
-
-@hydra.main(config_path="../config", config_name="gmm", version_base=None)
+@hydra.main(config_path="../config", config_name="gmm")
 def main(cfg: DictConfig) -> None:
-    # env = hydra.utils.instantiate(make_env())
-    env = None
+    # Run the following once to save the full env config
+    # with initialize(config_path="../config/calvin_env/"):
+    #     env_cfg = compose(config_name="default.yaml", overrides=["cameras=static_and_gripper"])
+    #     env_cfg.env["use_egl"] = False
+    #     env_cfg.env["show_gui"] = False
+    #     env_cfg.env["use_vr"] = False
+    #     env_cfg.env["use_scene_info"] = True
+    # pdb.set_trace()
+    env = hydra.utils.instantiate(cfg.calvin_env.env)
+    #     OmegaConf.save(config=cfg, f='../config/full_default.yaml')
+    # initialize(config_path="../config/")
+    # cfg = compose(config_name="gmm.yaml")
     trainer = GMMTrainer(cfg, env)
     trainer.run()
-
 
 
 if __name__ == '__main__':