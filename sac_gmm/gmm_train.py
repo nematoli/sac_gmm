96c1cb07f95f979b8ea86c7471252244275f3352
acl21 <research@akshaychandra.com>
2022-07-25 01:53:29 +0200

    does what it should, however doesn't do well on a custom env

--- sac_gmm/gmm_train.py
+++ sac_gmm/gmm_train.py
@@ -16,6 +16,9 @@ from utils.env_maker import make_env
 from gmm.gmr.gmm import GMM
 # from . import ModifiedGMM
 
+from gym import spaces
+from calvin_env.envs.play_table_env import PlayTableSimEnv
+
 import pdb
 
 logger = logging.getLogger(__name__)
@@ -44,15 +47,21 @@ class GMMTrainer(object):
         succesful_episodes, episodes_returns, episodes_lengths = 0, [], []
         for episode in tqdm(range(1, num_episodes + 1), desc="Evaluating GMM model"):
             observation = env.reset()
-            observation = observation['robot_obs'][:3]
-            action = gmm.priors
+            # pdb.set_trace()
+            # observation = observation['robot_obs'][:3]
+            observation = observation[:3]
+            pose = gmm.priors
             episode_return = 0
             for step in range(max_steps):
                 cgmm = gmm.condition([0, 1, 2], observation)
-                action = action + sampling_dt * cgmm.sample_confidence_region(1, alpha=0.7)[0]
-                pdb.set_trace()
+                pose = pose + sampling_dt * cgmm.sample_confidence_region(1, alpha=0.7)[0]
+                if step == 0:
+                    action = np.array([pose, np.zeros(3), -1], dtype=object)
+                else:
+                    action = np.append(pose, np.append(np.zeros(3), -1))
                 observation, reward, done, info = env.step(action)
-                observation = observation['robot_obs'][:3]
+                # observation = observation['robot_obs'][:3]
+                observation = observation[:3]
                 episode_return += reward
                 if render:
                     env.render()
@@ -92,7 +101,8 @@ class GMMTrainer(object):
         fitted_gmm = self.fit_gmm(X_train)
 
         # Evaluate in Calvin environment
-        self.evaluate(fitted_gmm, self.env)
+        acc, ep_returns, ep_lens = self.evaluate(fitted_gmm, self.env)
+        logger.info(f'Evaluation Results - Accuracy: {acc}, Avg. Episode Returns: {ep_returns}, Avg. Episode Lenths: {ep_lens}')
 
 @hydra.main(config_path="../config", config_name="gmm")
 def main(cfg: DictConfig) -> None:
@@ -104,12 +114,87 @@ def main(cfg: DictConfig) -> None:
     #     env_cfg.env["use_vr"] = False
     #     env_cfg.env["use_scene_info"] = True
     # pdb.set_trace()
-    env = hydra.utils.instantiate(cfg.calvin_env.env)
+    new_env_cfg = {**cfg.calvin_env.env}
+    new_env_cfg["tasks"] = cfg.calvin_env.tasks
+    new_env_cfg.pop('_target_', None)
+    new_env_cfg.pop('_recursive_', None)
+    env = TurnOffBulbEnv(**new_env_cfg)
     #     OmegaConf.save(config=cfg, f='../config/full_default.yaml')
     # initialize(config_path="../config/")
     # cfg = compose(config_name="gmm.yaml")
     trainer = GMMTrainer(cfg, env)
     trainer.run()
+
+
+
+
+class TurnOffBulbEnv(PlayTableSimEnv):
+    def __init__(self,
+                 tasks: dict = {},
+                 **kwargs):
+        super(TurnOffBulbEnv, self).__init__(**kwargs)
+        # For this example we will modify the observation to
+        # only retrieve the end effector pose
+        self.action_space = spaces.Box(low=-1, high=1, shape=(7,))
+        self.observation_space = spaces.Box(low=-1, high=1, shape=(7,))
+        # We can use the task utility to know if the task was executed correctly
+        self.tasks = hydra.utils.instantiate(tasks)
+
+    def reset(self):
+        obs = super().reset()
+        self.start_info = self.get_info()
+        return obs
+
+    def get_obs(self):
+        """Overwrite robot obs to only retrieve end effector position"""
+        robot_obs, robot_info = self.robot.get_observation()
+        return robot_obs[:7]
+
+    def _success(self):
+        """ Returns a boolean indicating if the task was performed correctly """
+        current_info = self.get_info()
+        task_filter = ["turn_off_lightbulb"]
+        task_info = self.tasks.get_task_info_for_set(self.start_info, current_info, task_filter)
+        return 'turn_off_lightbulb' in task_info
+
+    def _reward(self):
+        """ Returns the reward function that will be used 
+        for the RL algorithm """
+        reward = int(self._success()) * 10
+        r_info = {'reward': reward}
+        return reward, r_info
+
+    def _termination(self):
+        """ Indicates if the robot has reached a terminal state """
+        success = self._success()
+        done = success
+        d_info = {'success': success}        
+        return done, d_info
+
+    def step(self, action):
+            """ Performing a relative action in the environment
+                input:
+                    action: 7 tuple containing
+                            Position x, y, z. 
+                            Angle in rad x, y, z. 
+                            Gripper action
+                            each value in range (-1, 1)
+                output:
+                    observation, reward, done info
+            """
+            # Transform gripper action to discrete space
+            env_action = action.copy()
+            env_action[-1] = (int(action[-1] >= 0) * 2) - 1
+            self.robot.apply_action(env_action)
+            for i in range(self.action_repeat):
+                self.p.stepSimulation(physicsClientId=self.cid)
+            obs = self.get_obs()
+            info = self.get_info()
+            reward, r_info = self._reward()
+            done, d_info = self._termination()
+            info.update(r_info)
+            info.update(d_info)
+            return obs, reward, done, info
 
 
 if __name__ == '__main__':
