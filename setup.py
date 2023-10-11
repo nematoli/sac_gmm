63df237792c64e1f7de66d5c2577cb9914aa255d
Iman Nematollahi <nematoli@cs.uni-freiburg.de>
2023-10-11 10:29:19 +0200

    revert from ros to normal setup

--- setup.py
+++ setup.py
@@ -1,49 +1,38 @@
 """Setup sac_gmm installation."""
-# Try catkin install
-try:
-    from catkin_pkg.python_setup import generate_distutils_setup
-    from distutils.core import setup
+from os import path as op
+import re
+from setuptools import find_packages, setup
 
-    d = generate_distutils_setup(packages=["sac_gmm"], package_dir={"": "src"})
+def _read(f):
+    return open(op.join(op.dirname(__file__), f)).read() if op.exists(f) else ""
 
-    setup(**d)
+_meta = _read("sac_gmm/__init__.py")
 
-# Switch to regular pip install
-except ModuleNotFoundError:
-    from os import path as op
-    import re
-    from setuptools import find_packages, setup
+def find_meta(_meta, string):
+    l_match = re.search(r"^" + string + r'\s*=\s*"(.*)"', _meta, re.M)
+    if l_match:
+        return l_match.group(1)
+    raise RuntimeError(f"Unable to find {string} string.")
 
-    def _read(f):
-        return open(op.join(op.dirname(__file__), f)).read() if op.exists(f) else ""
+install_requires = [
+    l for l in _read("requirements.txt").split("\n") if l and not l.startswith("#") and not l.startswith("-")
+]
 
-    _meta = _read("sac_gmm/__init__.py")
+meta = dict(
+    name=find_meta(_meta, "__project__"),
+    version=find_meta(_meta, "__version__"),
+    license=find_meta(_meta, "__license__"),
+    description="Robot Skill Adaptation via Soft Actor-Critic Gaussian Mixture Models",
+    platforms=("Any"),
+    zip_safe=False,
+    keywords="pytorch sac_gmm".split(),
+    author=find_meta(_meta, "__author__"),
+    author_email=find_meta(_meta, "__email__"),
+    url=" https://github.com/nematoli/sac_gmm",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=install_requires,
+)
 
-    def find_meta(_meta, string):
-        l_match = re.search(r"^" + string + r'\s*=\s*"(.*)"', _meta, re.M)
-        if l_match:
-            return l_match.group(1)
-        raise RuntimeError(f"Unable to find {string} string.")
-
-    install_requires = [
-        l for l in _read("requirements.txt").split("\n") if l and not l.startswith("#") and not l.startswith("-")
-    ]
-
-    meta = dict(
-        name=find_meta(_meta, "__project__"),
-        version=find_meta(_meta, "__version__"),
-        license=find_meta(_meta, "__license__"),
-        description="Robot Skill Adaptation via Soft Actor-Critic Gaussian Mixture Models",
-        platforms=("Any"),
-        zip_safe=False,
-        keywords="pytorch sac_gmm".split(),
-        author=find_meta(_meta, "__author__"),
-        author_email=find_meta(_meta, "__email__"),
-        url=" https://github.com/nematoli/sac_gmm",
-        packages=find_packages(exclude=["tests"]),
-        install_requires=install_requires,
-    )
-
-    if __name__ == "__main__":
-        print("find_package", find_packages(exclude=["tests"]))
-        setup(**meta)
+if __name__ == "__main__":
+    print("find_package", find_packages(exclude=["tests"]))
+    setup(**meta)
