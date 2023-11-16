95eb47e840bc41843b08ff0a87d3a73f9cce009c
acl21 <research@akshaychandra.com>
2023-11-16 18:01:52 +0100

    play demos does not care about orientations now, the tilt is enough

--- sac_gmm/scripts/play_demos.py
+++ sac_gmm/scripts/play_demos.py
@@ -13,6 +13,7 @@ from pytorch_lightning.utilities import rank_zero_only
 from pytorch_lightning import seed_everything
 from sac_gmm.utils.utils import print_system_env_info, setup_logger
 from sac_gmm.utils.env_maker import make_env
+from sac_gmm.models.gmm.utils.rotation_utils import get_relative_quaternion
 
 cwd_path = Path(__file__).absolute().parents[0]
 sac_gmm_path = cwd_path.parents[0]
@@ -26,6 +27,8 @@ sys.path.insert(0, root.as_posix())  # root
 
 logger = logging.getLogger(__name__)
 
+import pybullet
+
 
 @rank_zero_only
 def log_rank_0(*args, **kwargs):
@@ -33,19 +36,31 @@ def log_rank_0(*args, **kwargs):
     logger.info(*args, **kwargs)
 
 
-def play(env, dataset, max_steps, render=False):
+def play(env, dataset, max_steps, record=False, render=False, out_dir=None):
     dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
     succesful_rollouts, failed_runs = 0, []
-    for idx, (xi, d_xi) in enumerate(dataloader):
+    for idx, (xi, d_xi, xi_ori) in enumerate(dataloader):
         log_rank_0(f"Trajectory {idx+1}/{len(dataset)}")
-
-        x0 = xi.squeeze()[0, :].numpy()
-        env.reset()
-        reached_target = env.calibrate_EE_start_state(desired_pos=x0)
+        pos = xi.squeeze().numpy()
+        ori = xi_ori.squeeze().numpy()
+        x0 = pos[0, :]
+        observation = env.reset()
+        reached_target = env.calibrate_EE_start_state(desired_pos=x0, max_checks=50)
+        observation = env.get_obs()
         if reached_target:
-            for step in range(max_steps):
-                d_x = d_xi.squeeze()[step, :].numpy()
-                observation, reward, done, info = env.step(d_x)
+            for step in range(1, len(pos)):
+                # Send relative actions to the env
+                next_pos = pos[step, :]
+                next_ori = ori[step, :]
+                dx_pos = (next_pos - observation["robot_obs"][:3]) / 0.02
+                # current_ori_quat = pybullet.getQuaternionFromEuler(observation["robot_obs"][3:6])
+                # dx_ori = get_relative_quaternion(current_ori_quat, next_ori)
+                # action = np.array([next_pos, next_ori, -1], dtype=object)
+                dx_ori = np.zeros(3)
+                action = np.append(dx_pos, np.append(dx_ori, -1))
+                observation, reward, done, info = env.step(action)
+                if record:
+                    env.record_frame(size=200)
                 if render:
                     env.render()
                 if done:
@@ -61,8 +76,13 @@ def play(env, dataset, max_steps, render=False):
             failed_runs.append(idx)
 
         log_rank_0(f"{idx+1}: {status}!")
+        if record:
+            log_rank_0("Saving Robot Camera Obs")
+            video_path = env.save_recording(outdir=out_dir, fname=idx)
+            env.reset_recording()
+            status = None
 
-    acc = succesful_rollouts / len(dataset.X)
+    acc = succesful_rollouts / len(dataset.X_pos)
 
     return acc, failed_runs
 
@@ -80,10 +100,18 @@ def play_demos(cfg: DictConfig) -> None:
     env.set_skill(cfg.skill)
 
     # Load dataset
+    cfg.datamodule.dataset.skill.skill = cfg.datamodule.dataset.skill.name
     train_dataset = hydra.utils.instantiate(cfg.datamodule.dataset)
-    log_rank_0(f"Skill: {cfg.skill.name}, Train Data: {train_dataset.X.size()}")
+    log_rank_0(f"Skill: {cfg.skill.name}, Train Data: {train_dataset.X_pos.size()}")
 
-    acc, failed = play(env, train_dataset, max_steps=cfg.skill.max_steps, render=cfg.render)
+    acc, failed = play(
+        env,
+        train_dataset,
+        max_steps=cfg.skill.max_steps,
+        record=cfg.record,
+        render=cfg.render,
+        out_dir=cfg.exp_dir,
+    )
     # Log evaluation output
     log_rank_0(f"{cfg.skill.name} Training Demos Accuracy: {round(acc, 2)}")
     if cfg.remove_failures:
@@ -92,13 +120,15 @@ def play_demos(cfg: DictConfig) -> None:
 
     cfg.datamodule.dataset.train = False
     val_dataset = hydra.utils.instantiate(cfg.datamodule.dataset)
-    log_rank_0(f"Skill: {cfg.skill.name}, Validation Data: {val_dataset.X.size()}")
+    log_rank_0(f"Skill: {cfg.skill.name}, Validation Data: {val_dataset.X_pos.size()}")
 
     acc, failed = play(
         env,
         val_dataset,
         max_steps=cfg.skill.max_steps,
+        record=cfg.record,
         render=cfg.render,
+        out_dir=cfg.exp_dir,
     )
     # Log evaluation output
     log_rank_0(f"{cfg.skill.name} Validation Demos Accuracy: {round(acc, 2)}")
